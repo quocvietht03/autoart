42cbcbc57e90e2edddc5fdb2587a18e8ec8712fb
tomdevvn90 <tomdevvn90@gmail.com>
2024-05-03 13:30:59 +0700

    update post style default

--- framework/templates/post-style.php
+++ framework/templates/post-style.php
@@ -1,12 +1,21 @@
 <article <?php post_class('bt-post'); ?>>
   <div class="bt-post--inner">
     <?php echo autoart_post_cover_featured_render($args['image-size']); ?>
-    <div class="bt-post--infor">
-      <?php
-        echo autoart_post_publish_render();
-        echo autoart_post_title_render();
-        echo autoart_post_short_meta_render();
-      ?>
+    <div class="bt-post--content">
+      <div class="bt-post--auto-detailing"> 
+        <a href="<?php echo esc_url(get_permalink()) ?>"> <?php esc_html_e( 'Auto Detailing', 'autoart' ); ?> </a>
+      </div>
+
+      <?php echo autoart_post_title_render(); ?>
+
+      <div class="bt-post--info"> 
+        <?php 
+          echo autoart_post_publish_render();
+          echo autoart_author_icon_render();
+        ?>
+      </div>
+
+      <?php echo autoart_post_button_render('View Details') ?>
     </div>
   </div>
 </article>
