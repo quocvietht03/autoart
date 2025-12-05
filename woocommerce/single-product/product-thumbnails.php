d6ca199c5f443037d5bedf1a6df5d0ffec202f06
tomdevvn90 <tomdevvn90@gmail.com>
2025-12-05 13:45:06 +0700

    update woocommerce old templates

--- woocommerce/single-product/product-thumbnails.php
+++ woocommerce/single-product/product-thumbnails.php
@@ -12,7 +12,7 @@
  *
  * @see         https://docs.woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     9.5.0
+ * @version     9.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
